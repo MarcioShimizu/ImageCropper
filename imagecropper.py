bdb9e731e561d5bd8cff0edab8918e312455317a
MacioShimizu <marcio.shimizu@hotmail.com>
2022-05-20 21:39:04 +0900

    organizando pastas

--- imagecropper.py
+++ imagecropper.py
@@ -1,16 +1,14 @@
 import cv2
-from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image import convert_from_path
 
 
-pdfname = 'BMW118D' 
+pdfname = 'atenza wagon' 
 images = convert_from_path(f'./pdf/{pdfname}.pdf',150)
 
 for i in range(len(images)):
+	images[i].save(f'./png/{pdfname}.png', 'PNG')
 
-	# Save pages as images in the pdf
-	images[i].save(f'{pdfname}.png', 'PNG')
-
-img = cv2.imread(f'{pdfname}.png')
+img = cv2.imread(f'./png/{pdfname}.png')
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
@@ -18,5 +16,5 @@ cropped = img[220:632, 810:1355]
 cv2.imshow('image', cropped)
 cv2.waitKey(0)
 
-cv2.imwrite(f'./{pdfname}-cropped.png', cropped)
+cv2.imwrite(f'./cropped-img/{pdfname}-cropped.png', cropped)
 cv2.destroyAllWindows()
